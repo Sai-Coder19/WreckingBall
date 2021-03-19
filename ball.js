de6c3e633352cb9496c5450350126072c8a1d76e
Sai-Coder19 <76458006+Sai-Coder19@users.noreply.github.com>
2021-03-19 19:54:54 -0400

    Add files via upload

---
+++ ball.js
@@ -0,0 +1,21 @@
+class Ball{
+    constructor(x,y,radius){
+        var options={
+            density:1,
+            frictionAir:0.005
+        }
+        this.body=Bodies.circle(x,y,radius,options);
+        this.radius=radius;
+        World.add(world,this.body);
+    }
+    display(){
+        var angle=this.body.angle;
+        var pos=this.body.position;
+        push();
+        translate(pos.x,pos.y);
+        rotate(angle);
+        ellipseMode(RADIUS);
+        ellipse(0,0,this.radius,this.radius);
+        pop();
+    }
+}
